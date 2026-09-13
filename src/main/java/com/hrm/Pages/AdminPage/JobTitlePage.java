0fc44624b3141015fb99485b978f7e751f3b3e57
HuyDeliver <channelbaby15@gmail.com>
2026-09-13 21:51:17 +0700

    Complete delete job titles

--- src/main/java/com/hrm/Pages/AdminPage/JobTitlePage.java
+++ src/main/java/com/hrm/Pages/AdminPage/JobTitlePage.java
@@ -1,9 +1,14 @@
 package com.hrm.Pages.AdminPage;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.hrm.Util.Log;
 import com.hrm.Util.componentLocator;
@@ -35,12 +40,42 @@ public class JobTitlePage {
     }
 
     // Edit job title
-    @FindBy(xpath = "//div[@class='oxd-table-card'][1]//div[contains(@class,'oxd-table-cell')][6]//button[2]")
-    private WebElement buttonEdit;
-
     public EditJobTitlePage goToEditPage() {
         waitUtils.waitElementClick(driver, componentLocator.editButtonTable(driver));
         return new EditJobTitlePage(driver);
     }
 
+    // Delete Job title
+
+    @FindBy(css = ".orangehrm-modal-header .oxd-text--card-title")
+    private WebElement deleteTitle;
+
+    @FindBy(css = ".orangehrm-modal-footer>button:nth-child(2)")
+    private WebElement confirmDelete;
+
+    @FindBy(css = ".oxd-text.oxd-text--p.oxd-text--toast-message.oxd-toast-content-text")
+    private WebElement deleteSuccess;
+
+    public void clickDelete() {
+        waitUtils.waitElementClick(driver, componentLocator.deleteButtonTable(driver));
+
+        By deleteModal = By
+                .cssSelector(".oxd-dialog-container-default .oxd-sheet");
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(deleteModal));
+    }
+
+    public boolean isModalDeleteVisible() {
+        Log.info("kiểm tra modal có hiện ko");
+        return deleteTitle.isDisplayed();
+    }
+
+    public void deleteUserInModal() {
+        Log.info("Xác nhận xóa");
+        waitUtils.waitElementClick(driver, confirmDelete);
+    }
+
+    public boolean isDeleteSuccess() {
+        return deleteSuccess.isDisplayed();
+    }
 }
---
+++ src/test/java/com/hrm/TestCase/AdminTab/DeleteJobTitlesFunctionality.java
@@ -0,0 +1,31 @@
+package com.hrm.TestCase.AdminTab;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.hrm.Base.TestBase;
+import com.hrm.Base.TestUtil;
+import com.hrm.Pages.AdminPage.JobTitlePage;
+import com.hrm.Util.Log;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+
+@Epic("Module AdminTab")
+@Feature("Quản lý Job Title")
+public class DeleteJobTitlesFunctionality extends TestBase {
+    @Test(description = "OHR11: Delete Job")
+    public void deleteJob() {
+        TestUtil.jobUtil();
+        JobTitlePage jobTitlePage = new JobTitlePage(driver);
+        Assert.assertTrue(jobTitlePage.isJobtitleVisible(), "Không vào được job title");
+
+        jobTitlePage.clickDelete();
+        Assert.assertTrue(jobTitlePage.isModalDeleteVisible(), "Không hiện modal xóa");
+        jobTitlePage.deleteUserInModal();
+
+        Assert.assertTrue(jobTitlePage.isDeleteSuccess(), "Delete không thành công");
+
+        Log.info("Delete Job title success");
+    }
+}
